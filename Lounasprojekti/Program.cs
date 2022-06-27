9a1f7d1dcbc1e05c9ec64677044138a4cb269d12
Jere Rytkönen <jere.rytkonen@awacademy.training>
2022-06-27 14:26:46 +0200

    HaeRavintolaTiedot ja HaeRuokailijat lisätty

--- Lounasprojekti/Program.cs
+++ Lounasprojekti/Program.cs
@@ -11,7 +11,6 @@ Kyselyt a = new Kyselyt();
 var demo = a.SelaaRavintolat();
 foreach (var item in demo)
     Console.WriteLine(item.Key + " " + item.Value);
-Console.WriteLine(demo[]);
 
 class Kyselyt
 {
@@ -25,16 +24,46 @@ class Kyselyt
         // lisätään where ehto näyttämään vain tälle päivälle
         Dictionary<string, int> map = new Dictionary<string, int>();
         var kysely = (from i in db.Ravintolas
-                     join i2 in db.Lounastapahtumas on i.RavintolaId equals i2.RavintolaId
-                     join i3 in db.Lounasseuras on i2.LounastapahtumaId equals i3.LounastapahtumaId
-                     group i by i.RavintolanNimi into g
-                     select new { Ravintolanimi = g.Key, Count = g.Count() }).ToList();
+                      join i2 in db.Lounastapahtumas on i.RavintolaId equals i2.RavintolaId
+                      join i3 in db.Lounasseuras on i2.LounastapahtumaId equals i3.LounastapahtumaId
+                      group i by i.RavintolanNimi into g
+                      select new { Ravintolanimi = g.Key, Count = g.Count() }).ToList();
         foreach (var item in kysely)
         {
             map.Add(item.Ravintolanimi, item.Count);
         }
         return map;
     }
+public List<string> HaeRavintolanTiedot(int ravintolaID)
+    {
+        // Lisää ravintolan kommentit listan loppuun
+        var ravintola = db.Ravintolas.Find(ravintolaID);
+        var kysely1 = (from i in db.Arvios
+                       where i.RavintolaId == ravintolaID
+                       select i.Arvosana).Average();
 
+        var lista = new List<string>();
+        lista.Add(ravintola.RavintolanNimi);
+        lista.Add(ravintola.Kategoria);
+        lista.Add(kysely1.ToString());
+        lista.Add(ravintola.Osoite);
+        lista.Add(ravintola.Postinumero);
+        lista.Add(ravintola.Postitoimipaikka);
+        lista.Add(ravintola.Verkkosivu);
+        return lista;
+    }
+    public void HaeRuokailijat(int ravintolaID)
+    {
+        var kysely = from i in db.Lounastapahtumas
+                     join i2 in db.Lounasseuras on i.LounastapahtumaId equals i2.LounastapahtumaId
+                     join i3 in db.Käyttäjäs on i2.KäyttäjäId equals i3.KäyttäjäId
+                     where i.RavintolaId == ravintolaID
+                     select i;
+        foreach (var item in kysely)
+        {
+            Console.WriteLine(item);
+        }
+
+    }
 
 }
