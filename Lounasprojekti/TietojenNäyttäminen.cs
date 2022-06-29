bcea5b7aaed53c7e67129c1f2e00d26502a608e8
Jere Rytkönen <jere.rytkonen@awacademy.training>
2022-06-29 08:34:52 +0200

    TietojenNäyttäminen luokka siistitty

--- Lounasprojekti/TietojenNäyttäminen.cs
+++ Lounasprojekti/TietojenNäyttäminen.cs
@@ -9,30 +9,22 @@ static class TietojenNäyttäminen
 
     static LounasDBContext db = new LounasDBContext();
 
-    public static void NäytäRavintolat(Dictionary<string, int> a)
+    public static void NäytäRavintolanTiedot(List<string> ravintolanTiedot)
     {
-        
-        foreach (var item in a)
-            Console.WriteLine(item.Key + " " + item.Value);
-        Console.ReadLine();
-    }
-
-    public static void NäytäRavintolanTiedot(List<string> a)
-    {
-        PäivitäRavintolaId(a[0]);
-        RavintolaNimi = a[0];
-        foreach (var item in a)
+        PäivitäRavintolaId(ravintolanTiedot[0]);
+        RavintolaNimi = ravintolanTiedot[0];
+        foreach (var item in ravintolanTiedot)
             Console.WriteLine(item);
         Console.ReadLine();
     }
 
 
-    public static void NäytäRavintolanTiedot(List<string> a, ConsoleMenu con)
+    public static void NäytäRavintolanTiedot(List<string> ravintolanTiedot, ConsoleMenu con)
     {
-        PäivitäRavintolaId(a[0]);
-        RavintolaNimi = a[0];
+        PäivitäRavintolaId(ravintolanTiedot[0]);
+        RavintolaNimi = ravintolanTiedot[0];
         Console.Clear();
-        foreach (var item in a)
+        foreach (var item in ravintolanTiedot)
             Console.WriteLine(item);
         Console.ReadLine();
         con.Show();
