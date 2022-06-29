441d1e22d6b64413f103bdf2890da0f01317d835
Jere Rytkönen <jere.rytkonen@awacademy.training>
2022-06-29 09:32:58 +0200

    Siirretty valikko omaan luokkaan.

--- Lounasprojekti/Program.cs
+++ Lounasprojekti/Program.cs
@@ -6,91 +6,23 @@ using Microsoft.EntityFrameworkCore.Metadata;
 using Lounasprojekti;
 using ConsoleTools;
 
-
+var valikot = new Valikot();
 var kirjautuminen = new Kirjautuminen();
+var ravintolatSubMenu = valikot.ravintolaSubMenu(args, kirjautuminen);
+var ravintolatMenu = valikot.ravintolatMenu(args, kirjautuminen, ravintolatSubMenu);
+
 kirjautuminen.Kirjaudu();
-
-
-var a = new Kyselyt();
-var b = new Muokkaus();
-//var c = new TietojenNäyttäminen();
-
-//var demo = a.SelaaRavintolat();
-//foreach (var item in demo)
-//    Console.WriteLine(item.Key + " " + item.Value);
-//a.HaeRuokailijat(1);
-
-//b.LisääArvio(2, 1, 4, "Olipas hyvää.");
-
-var ravintolaSubMenu = new ConsoleMenu(args, level: 2)
-    .Add("Ilmoittaudu lounasseuraksi", () => b.IlmoittauduLounaalle(TietojenNäyttäminen.RavintolaID, kirjautuminen.KäyttäjäId, DateTime.Today.AddDays(1)))
-    .Add("Arvioi lounasravintola", () => b.LisääArvio(TietojenNäyttäminen.RavintolaID, kirjautuminen.KäyttäjäId))
-  //.Add("Sub_One",() => NäytäRavintolat(a.SelaaRavintolat()))
-  .Add("Sub_Close", ConsoleMenu.Close)
-  .Configure(config =>
-  {
-      config.Selector = "--> ";
-      config.EnableFilter = false;
-      config.Title = "Submenu";
-      config.EnableBreadcrumb = true;
-      config.WriteBreadcrumbAction = titles => Console.WriteLine(string.Join(" / ", titles));
-  });
-
-var ravintolatMenu = new ConsoleMenu(args, level: 1)
-    .AddRange(a.SelaaRavintolatValikko(ravintolaSubMenu))
-
-  //.Add("Sub_One",() => NäytäRavintolat(a.SelaaRavintolat()))
-  .Add("Sub_Close", ConsoleMenu.Close)
-  .Configure(config =>
-  {
-      config.Selector = "--> ";
-      config.EnableFilter = false;
-      config.Title = "Submenu";
-      config.EnableBreadcrumb = true;
-      config.WriteBreadcrumbAction = titles => Console.WriteLine(string.Join(" / ", titles));
-  });
-
-var käyttäjäMenu = new ConsoleMenu(args, level: 0)
-  .Add("Näytä ravintolat", ravintolatMenu.Show)
-  .Add("Change me", (thisMenu) => thisMenu.CurrentItem.Name = "I am changed!")
-  .Add("Close", ConsoleMenu.Close)
-  //.Add("Action then Close", (thisMenu) => { SomeAction("Close"); thisMenu.CloseMenu(); })
-  .Add("Exit", () => Environment.Exit(0))
-  .Configure(config =>
-  {
-      config.Selector = "--> ";
-      config.EnableFilter = false;
-      config.Title = "Main menu";
-      config.EnableWriteTitle = true;
-      config.EnableBreadcrumb = true;
-  });
-
-var adminMenu = new ConsoleMenu(args, level: 0)
-  .Add("Näytä ravintolat", ravintolatMenu.Show)
-  .Add("Lisää ravintola", () => b.LisääRavintola())
-  .Add("Change me", (thisMenu) => thisMenu.CurrentItem.Name = "I am changed!")
-  .Add("Close", ConsoleMenu.Close)
-  //.Add("Action then Close", (thisMenu) => { SomeAction("Close"); thisMenu.CloseMenu(); })
-  .Add("Exit", () => Environment.Exit(0))
-  .Configure(config =>
-  {
-      config.Selector = "--> ";
-      config.EnableFilter = false;
-      config.Title = "Main menu";
-      config.EnableWriteTitle = true;
-      config.EnableBreadcrumb = true;
-  });
-
-
 
 if (kirjautuminen.OnAdmin == 0)
 {
-käyttäjäMenu.Show();
+    var käyttäjäMenu = valikot.käyttäjäMenu(args, ravintolatMenu);
+    käyttäjäMenu.Show();
 }
 
 if (kirjautuminen.OnAdmin == 1)
 {
-adminMenu.Show();
+    var adminMenu = valikot.adminMenu(args, ravintolatMenu);
+    adminMenu.Show();
 }
 
 
---
+++ Lounasprojekti/Valikot.cs
@@ -0,0 +1,82 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleTools;
+
+class Valikot
+{
+
+    public ConsoleMenu ravintolaSubMenu(string[] args, Kirjautuminen kirjautuminen)
+    {
+        var kyselyObjekti = new Kyselyt();
+        var muokkausObjekti = new Muokkaus();
+        var ravintolaSubMenu = new ConsoleMenu(args, level: 2)
+            .Add("Ilmoittaudu lounasseuraksi", () => muokkausObjekti.IlmoittauduLounaalle(TietojenNäyttäminen.RavintolaID, kirjautuminen.KäyttäjäId, DateTime.Today.AddDays(1)))
+            .Add("Arvioi lounasravintola", () => muokkausObjekti.LisääArvio(TietojenNäyttäminen.RavintolaID, kirjautuminen.KäyttäjäId))
+            .Add("Sub_Close", ConsoleMenu.Close)
+            .Configure(config =>
+    {
+        config.Selector = "--> ";
+        config.EnableFilter = false;
+        config.Title = "Submenu";
+        config.EnableBreadcrumb = true;
+        config.WriteBreadcrumbAction = titles => Console.WriteLine(string.Join(" / ", titles));
+    });
+        return ravintolaSubMenu;
+    }
+
+    public ConsoleMenu ravintolatMenu(string[] args, Kirjautuminen kirjautuminen, ConsoleMenu ravintolatSubMenu)
+    {
+        var kyselyObjekti = new Kyselyt();
+        var muokkausObjekti = new Muokkaus();
+        var ravintolatMenu = new ConsoleMenu(args, level: 1)
+          .AddRange(kyselyObjekti.SelaaRavintolatValikko(ravintolatSubMenu))
+          .Add("Sub_Close", ConsoleMenu.Close)
+          .Configure(config =>
+      {
+          config.Selector = "--> ";
+          config.EnableFilter = false;
+          config.Title = "Submenu";
+          config.EnableBreadcrumb = true;
+          config.WriteBreadcrumbAction = titles => Console.WriteLine(string.Join(" / ", titles));
+      });
+        return ravintolatMenu;
+    }
+    public ConsoleMenu käyttäjäMenu(string[] args, ConsoleMenu ravintolatMenu)
+    {
+        var kyselyObjekti = new Kyselyt();
+        var muokkausObjekti = new Muokkaus();
+        var käyttäjäMenu = new ConsoleMenu(args, level: 0)
+          .Add("Näytä ravintolat", ravintolatMenu.Show)
+          .Add("Change me", (thisMenu) => thisMenu.CurrentItem.Name = "I am changed!")
+          //.Add("Close", ConsoleMenu.Close)
+          .Add("Exit", () => Environment.Exit(0))
+      .Configure(config =>
+      {
+          config.Selector = "--> ";
+          config.EnableFilter = false;
+          config.Title = "Main menu";
+          config.EnableWriteTitle = true;
+          config.EnableBreadcrumb = true;
+      }); 
+        return käyttäjäMenu;
+    }
+    public ConsoleMenu adminMenu(string[] args, ConsoleMenu ravintolatMenu)
+    {
+        var kyselyObjekti = new Kyselyt();
+        var muokkausObjekti = new Muokkaus();
+        var adminMenu = new ConsoleMenu(args, level: 0)
+            .Add("Näytä ravintolat", ravintolatMenu.Show)
+            .Add("Lisää ravintola", () => muokkausObjekti.LisääRavintola())
+            .Add("Change me", (thisMenu) => thisMenu.CurrentItem.Name = "I am changed!")
+            .Add("Exit", () => Environment.Exit(0))
+  .Configure(config =>
+  {
+      config.Selector = "--> ";
+      config.EnableFilter = false;
+      config.Title = "Main menu";
+      config.EnableWriteTitle = true;
+      config.EnableBreadcrumb = true;
+  });
+        return adminMenu;
+    }
+}
+
