399589c69a350918f9a28e21fd2cfdc18161eb13
Jere Rytkönen <jere.rytkonen@awacademy.training>
2022-07-22 14:17:10 +0300

    Muutettu takaisin toimimaan lokaalisti

---
+++ Käyttöliittymä/Models/Käyttäjä.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Käyttöliittymä.Models
+{
+    public partial class Käyttäjä
+    {
+        public Käyttäjä()
+        {
+            Arvios = new HashSet<Arvio>();
+            Lounasseuras = new HashSet<Lounasseura>();
+        }
+
+        public int KäyttäjäId { get; set; }
+        public string Käyttäjänimi { get; set; } = null!;
+        public int? OnAdmin { get; set; }
+
+        public virtual ICollection<Arvio> Arvios { get; set; }
+        public virtual ICollection<Lounasseura> Lounasseuras { get; set; }
+    }
+}
--- Käyttöliittymä/Models/LounasDBContext.cs
+++ Käyttöliittymä/Models/LounasDBContext.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
-namespace LounasprojektiLib.Models
+namespace Käyttöliittymä.Models
 {
     public partial class LounasDBContext : DbContext
     {
